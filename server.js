acb7019b3083db6e7557a6b2498c12eb55961b1f
necmigunduz <necmigunduz@gmail.com>
2021-12-31 17:33:37 +0300

    Add server

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-import express from 'express'
-import express_graphql from 'express-graphql'
-import { buildSchema } from 'graphql'
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-var schema = buildSchema (`
+// Schema
+var schema = buildSchema(`
     type Query {
-        course(id: Int!): Course
-        course(topic: String): [Course]
-    },
-    type Course {
-        id: Int
-        title: String
-        author: String
-        description: String
-        topic: String
-        url: String
+        message: String
     }
 `)
 
-var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-]
+// Root
+var root = {
+    message: () => 'This is a try-out!'
+}
+
+// Create app
+var app = express()
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+var port = process.env.PORT || 4000
+app.listen(port, ()=> console.log(`Express Graphql Server is running on the port of ${port}`))
